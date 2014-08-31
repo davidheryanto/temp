e7414be64592937a1fad0bc242e13250a2714752
David Heryanto <david.heryanto@hotmail.com>
2014-09-01 01:33:46 +0800

    Clean up unused stuff

--- test_optime/test_cross_validation.py
+++
@@ -1,9 +0,0 @@
-from sklearn import cross_validation
-from sklearn import datasets
-from sklearn import svm
-
-iris = datasets.load_iris()
-clf = svm.SVC(kernel='linear', C=1)
-scores = cross_validation.cross_val_score(clf, iris.data, iris.target, cv=5)
-
-print(scores)
